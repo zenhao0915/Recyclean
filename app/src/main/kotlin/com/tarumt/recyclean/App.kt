057a7454b13099cd9145fbfb8f98f3562981c506
zenhao <zenhao5514@gmail.com>
2026-06-29 16:26:36 +0800

    Structure For App Main

---
+++ app/src/main/kotlin/com/tarumt/recyclean/App.kt
@@ -0,0 +1,19 @@
+package com.tarumt.recyclean
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.Black)) {
+
+    }
+}
--- app/src/main/kotlin/com/tarumt/recyclean/MainActivity.kt
+++ app/src/main/kotlin/com/tarumt/recyclean/MainActivity.kt
@@ -17,31 +17,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-            RecycleanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            App()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecycleanTheme {
-        Greeting("Android")
     }
 }
