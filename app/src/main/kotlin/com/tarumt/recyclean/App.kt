a24f43748af70f20c2899b842c4cf2b654712167
zenhao <zenhao5514@gmail.com>
2026-06-29 17:28:13 +0800

    Main Page Test

--- app/src/main/kotlin/com/tarumt/recyclean/App.kt
+++ app/src/main/kotlin/com/tarumt/recyclean/App.kt
@@ -1,19 +1,35 @@
 package com.tarumt.recyclean
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.tarumt.recyclean.common.defaultFont
 
 @Composable
 @Preview
 fun App() {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(color = Color.Black)) {
-
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black)
+    ) {
+        Row(
+            Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier.background(Color.White), text = "Test", fontFamily = defaultFont
+            )
+        }
     }
 }
