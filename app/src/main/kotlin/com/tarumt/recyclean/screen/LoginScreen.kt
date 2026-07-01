0b1b0b7dc297513f396417f1a71b658f9529d68d
zenhao <zenhao5514@gmail.com>
2026-07-01 17:07:37 +0800

    Login Page Preparation

--- app/src/main/kotlin/com/tarumt/recyclean/screen/LoginScreen.kt
+++ app/src/main/kotlin/com/tarumt/recyclean/screen/LoginScreen.kt
@@ -1,8 +1,36 @@
 package com.tarumt.recyclean.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.tarumt.recyclean.R
+import com.tarumt.recyclean.common.defaultBoldFont
 
 @Composable
-fun LoginScreen() {
-
+@Preview
+fun LoginScreen() = Box(
+    modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.White),
+    contentAlignment = Alignment.Center
+) {
+    Column(
+        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(R.drawable.logo), contentDescription = null)
+        Text(text = "User Login", fontFamily = defaultBoldFont, fontSize = 32.sp)
+        Row() { }
+    }
 }
