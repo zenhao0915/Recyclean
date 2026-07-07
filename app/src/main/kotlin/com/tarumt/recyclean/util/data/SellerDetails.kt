4b16392818fd205fb19ae746895ff9e3ef173dbc
zenhao <zenhao5514@gmail.com>
2026-07-07 10:58:43 +0800

    Seller Logo

--- app/src/main/kotlin/com/tarumt/recyclean/util/data/SellerDetails.kt
+++ app/src/main/kotlin/com/tarumt/recyclean/util/data/SellerDetails.kt
@@ -7,16 +7,15 @@ enum class Sellers(
     val gradeDetails: Grade,
     val sellerLogo: Int = R.drawable.ic_launcher_background
 ) {
-    SenHeng("SenHeng", Grade.S_Plus),
-    TNB("TNB", Grade.S_Plus),
-    CompAsia("CompAsia", Grade.S),
-    ERTH("ERTH", Grade.A),
-    Shan_Poornam_Metals("Shan Poornam Metals", Grade.A),
-    TES_AMM("TES AMM", Grade.B),
-    Reebelo("Reebelo", Grade.A),
-    Virogreen("Virogreen", Grade.B),
-    Rentwise("Rentwise", Grade.C),
-    PC_Image("PC Image", Grade.A),
-    JMI("JMI", Grade.B),
-    KPT_Recycle("KPT Recycle", Grade.A)
+    SenHeng("SenHeng", Grade.S_Plus, R.drawable.senheng_logo),
+    TNB("TNB", Grade.S_Plus, R.drawable.tnb_logo),
+    CompAsia("CompAsia", Grade.S, R.drawable.compasia_logo),
+    ERTH("ERTH", Grade.A, R.drawable.erth_logo),
+    Shan_Poornam_Metals("Shan Poornam Metals", Grade.A, R.drawable.spm_logo),
+    TES_AMM("TES AMM", Grade.B, R.drawable.sk_tes),
+    Reebelo("Reebelo", Grade.A, R.drawable.reebelo_logo),
+    Virogreen("Virogreen", Grade.B, R.drawable.virogreen_logo),
+    Rentwise("Rentwise", Grade.C, R.drawable.rentwise_png),
+    PC_Image("PC Image", Grade.B, R.drawable.pc_image_logo),
+    KPT_Recycle("KPT Recycle", Grade.A, R.drawable.kpt_logo)
 }
