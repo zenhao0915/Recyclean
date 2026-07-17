e9a96bde1bbe2fc80b82890a6475398eb6a788dd
zenhao_ <zenhao5514@gmail.com>
2026-07-17 15:52:13 +0800

    Migrate AppoinmentData.kt To Data Package

---
+++ app/src/main/kotlin/com/tarumt/recyclean/util/data/AppoinmentData.kt
@@ -0,0 +1,19 @@
+package com.tarumt.recyclean.util.data
+
+import com.tarumt.recyclean.screen.SalvageablePart
+
+data class Appointment(
+    val appointmentId: String,
+    val userName: String,
+    val deviceName: String,
+    val scheduledDate: String,
+    val estimatedValue: Double,
+    val status: AppointmentStatus,
+    val selectedParts: List<SalvageablePart>,
+    val targetSeller: String
+)
+
+enum class AppointmentStatus {
+    PENDING, REVIEWED, COMPLETED
+}
+
