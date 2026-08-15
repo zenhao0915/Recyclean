1b2a2198ec1aec70600af831ea821f10b077377b
zenhao <zenhao5514@gmail.com>
2026-08-15 20:29:13 +0800

    APK Build Support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.tarumt.recyclean"
-        minSdk = 36
+        minSdk = 28
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
