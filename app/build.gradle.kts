d1251d8562b3caf67091c2cee07a419bcd738c7f
zenhao <zenhao5514@gmail.com>
2026-08-25 14:16:33 +0800

    Update Not Used Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.androidx.navigation.compose)
     implementation(libs.compose.material.icons.core)
     implementation(libs.compose.material.icons.extended)
     // Google Map API
